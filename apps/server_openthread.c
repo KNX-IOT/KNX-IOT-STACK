a52239382621ff97dc80edb3c5edfaa6e3bf884d
Wouter <wavdb66@gmail.com>
2021-09-21 14:31:43 +0100

    removal of iotivity

--- apps/server_openthread.c
+++ apps/server_openthread.c
@@ -179,7 +179,7 @@ main(int argc, char *argv[])
 
   otSetStateChangedCallback(ot_instance, ot_state_changed, NULL);
 
-  // init iotivity
+  // init the stack
 
   static const oc_handler_t handler = { .init = app_init,
                                         .signal_event_loop = signal_event_loop,
