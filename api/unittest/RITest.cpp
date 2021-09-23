00e4147d26a0a8a083c3f7223a75ee693afe0e25
Wouter <wavdb66@gmail.com>
2021-09-23 16:46:46 +0100

    fix for unit tests

--- api/unittest/RITest.cpp
+++ api/unittest/RITest.cpp
@@ -137,8 +137,8 @@ TEST_F(TestOcRi, RiAddResource_P)
 TEST_F(TestOcRi, RIGetQueryValue_P)
 {
   const char *input[] = { "key=1",          "data=1&key=2",     "key=2&data=3",
-                          "x&key=2&data=3", "y&x&key=2&data=3", "y&x&key=2",
-                          "y&x&key=2&y" };
+                          "key=2&data=3", "key=2&data=3", "key=2",
+                          "key=2&y" };
   int ret;
   char *value;
 
@@ -185,42 +185,42 @@ TEST_F(TestOcRi, RIinterfacestring_P)
   const char * interface;
   
   interface = get_interface_string(OC_IF_I);
-  EXPECT_EQ("if.i", interface);
+  EXPECT_STREQ("if.i", interface);
   
   interface = get_interface_string(OC_IF_O);
-  EXPECT_EQ("if.o", interface);
+  EXPECT_STREQ("if.o", interface);
   
   //interface = get_interface_string(OC_IF_G);
   //EXPECT_EQ("if.i", interface);
   
   interface = get_interface_string(OC_IF_C);
-  EXPECT_EQ("if.c", interface);
+  EXPECT_STREQ("if.c", interface);
     
   interface = get_interface_string(OC_IF_P);
-  EXPECT_EQ("if.p", interface);  
+  EXPECT_STREQ("if.p", interface);  
   
   interface = get_interface_string(OC_IF_D);
-  EXPECT_EQ("if.d", interface);
+  EXPECT_STREQ("if.d", interface);
   
   interface = get_interface_string(OC_IF_AC);
-  EXPECT_EQ("if.a", interface);
+  EXPECT_STREQ("if.a", interface);
   
   interface = get_interface_string(OC_IF_SE);
-  EXPECT_EQ("if.s", interface);
+  EXPECT_STREQ("if.s", interface);
   
   interface = get_interface_string(OC_IF_LIL);
-  EXPECT_EQ("if.ll", interface);
+  EXPECT_STREQ("if.ll", interface);
   
   interface = get_interface_string(OC_IF_BA);
-  EXPECT_EQ("if.b", interface);
+  EXPECT_STREQ("if.b", interface);
   
   interface = get_interface_string(OC_IF_SEC);
-  EXPECT_EQ("if.sec", interface);
+  EXPECT_STREQ("if.sec", interface);
   
   interface = get_interface_string(OC_IF_SWU);
-  EXPECT_EQ("if.swu", interface);
+  EXPECT_STREQ("if.swu", interface);
   
   interface = get_interface_string(OC_IF_PM);
-  EXPECT_EQ("if.pm", interface);
+  EXPECT_STREQ("if.pm", interface);
   
 }
