fe49aa696cd33877a84bfc6bf151af7cf1d76786
Wouter <wavdb66@gmail.com>
2021-09-24 17:09:24 +0100

    added copyrights

--- port/linux/tcpadapter.c
+++ port/linux/tcpadapter.c
@@ -1,6 +1,7 @@
 /****************************************************************************
  *
- * Copyright 2018 Samsung Electronics All Rights Reserved.
+ * Copyright (c) 2018 Samsung Electronics
+ * Copyright (c) 2021 Cascoda Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
