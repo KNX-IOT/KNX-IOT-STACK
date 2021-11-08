77b6910213f02f9d715ff374b97918acf1a4d990
Wouter <wavdb66@gmail.com>
2021-11-08 17:23:44 +0000

    documentation & clean up

--- api/oc_client_api.c
+++ api/oc_client_api.c
@@ -335,7 +335,7 @@ oc_do_delete(const char *uri, oc_endpoint_t *endpoint, const char *query,
 
   if (status)
     status =
-      dispatch_coap_request(APPLICATION_VND_OCF_CBOR, APPLICATION_VND_OCF_CBOR);
+      dispatch_coap_request(APPLICATION_CBOR, APPLICATION_CBOR);
 
   return status;
 }
