8d297d3b5ac4cdf2de26fb8f9a9a9fe39ba84479
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-11-30 12:32:58 +0000

    dummy C->Python bindings

---
+++ apps/simpleclient_displayotron.c
@@ -0,0 +1,35 @@
+#include <Python.h>
+
+static int numargs=0;
+
+/* Return the number of arguments of the application command line */
+static PyObject*
+emb_numargs(PyObject *self, PyObject *args)
+{
+    if(!PyArg_ParseTuple(args, ":numargs"))
+        return NULL;
+    return PyLong_FromLong(numargs);
+}
+
+static PyMethodDef EmbMethods[] = {
+    {"numargs", emb_numargs, METH_VARARGS,
+     "Return the number of arguments received by the process."},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyModuleDef EmbModule = {
+    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods,
+    NULL, NULL, NULL, NULL
+};
+
+static PyObject*
+PyInit_emb(void)
+{
+    return PyModule_Create(&EmbModule);
+}
+
+int main(void) {
+	PyImport_AppendInittab("emb", PyInit_emb);
+	Py_Initialize();
+	return 0;
+}
