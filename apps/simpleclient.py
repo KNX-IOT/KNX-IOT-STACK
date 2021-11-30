b86dbe92542f9203ca1ae1f17cb7f56eefb20807
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-11-30 13:06:06 +0000

    slower twinkle

--- apps/simpleclient.py
+++ apps/simpleclient.py
@@ -4,6 +4,8 @@ import dothat.backlight as backlight
 import dothat.lcd as lcd
 import dothat.touch as touch
 
+from time import sleep
+
 IDLE_BL = (128, 128, 128)
 DARK_BL = (0, 0, 0)
 FULL_BL = (0, 255, 0)
@@ -16,22 +18,22 @@ def init():
 
 @touch.on(touch.LEFT)
 def handle_left(ch, evt):
-    backlight.rgb(*DARK_BL)
     backlight.left_rgb(*FULL_BL)
+    sleep(0.01)
     # knx_handle_left()
     backlight.rgb(*IDLE_BL)
 
 @touch.on(touch.BUTTON)
 def handle_mid(ch, evt):
-    backlight.rgb(*DARK_BL)
     backlight.mid_rgb(*FULL_BL)
+    sleep(0.01)
     # knx_handle_mid()
     backlight.rgb(*IDLE_BL)
 
 @touch.on(touch.RIGHT)
 def handle_right(ch, evt):
-    backlight.rgb(*DARK_BL)
     backlight.right_rgb(*FULL_BL)
+    sleep(0.01)
     # knx_handle_right()
     backlight.rgb(*IDLE_BL)
 
