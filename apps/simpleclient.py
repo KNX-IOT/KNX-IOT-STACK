c616516ca78e85c5c61ff7965efc2f8bc67084d5
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-11-30 15:04:22 +0000

    fully working Python->C integration for left button

--- apps/simpleclient.py
+++ apps/simpleclient.py
@@ -4,11 +4,14 @@ import dothat.backlight as backlight
 import dothat.lcd as lcd
 import dothat.touch as touch
 
+import knx
+
 from time import sleep
 
 IDLE_BL = (128, 128, 128)
 DARK_BL = (0, 0, 0)
 FULL_BL = (0, 255, 0)
+BLINK_TIME_S = 0.03
 
 def init():
     backlight.rgb(*IDLE_BL)
@@ -19,24 +22,25 @@ def init():
 @touch.on(touch.LEFT)
 def handle_left(ch, evt):
     backlight.left_rgb(*FULL_BL)
-    sleep(0.01)
-    # knx_handle_left()
+    knx.handle_left()
+    sleep(BLINK_TIME_S)
     backlight.rgb(*IDLE_BL)
 
 @touch.on(touch.BUTTON)
 def handle_mid(ch, evt):
     backlight.mid_rgb(*FULL_BL)
-    sleep(0.01)
     # knx_handle_mid()
+    sleep(BLINK_TIME_S)
     backlight.rgb(*IDLE_BL)
 
 @touch.on(touch.RIGHT)
 def handle_right(ch, evt):
     backlight.right_rgb(*FULL_BL)
-    sleep(0.01)
     # knx_handle_right()
+    sleep(BLINK_TIME_S)
     backlight.rgb(*IDLE_BL)
 
 if __name__ == "__main__":
     init()
+    print("Pausing from within Python main")
     signal.pause()
--- apps/simpleclient_displayotron.c
+++ apps/simpleclient_displayotron.c
@@ -2,7 +2,7 @@
 
 static int numargs=0;
 
-/* Return the number of arguments of the application command line */
+/* Return the number of arguments of the application command line 
 static PyObject*
 emb_numargs(PyObject *self, PyObject *args)
 {
@@ -10,26 +10,59 @@ emb_numargs(PyObject *self, PyObject *args)
         return NULL;
     return PyLong_FromLong(numargs);
 }
+*/
 
-static PyMethodDef EmbMethods[] = {
-    {"numargs", emb_numargs, METH_VARARGS,
-     "Return the number of arguments received by the process."},
+/* Action to take on left button press */
+static PyObject*
+knx_handle_left(PyObject *self, PyObject *args)
+{
+    // don't care about args, so don't check them
+    (void) self;
+    (void) args;
+    printf("Left button press received within C!\n");
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef KnxMethods[] = {
+    {"handle_left", knx_handle_left, METH_VARARGS,
+     "Inform the KNX stack that the left button has been pressed."},
     {NULL, NULL, 0, NULL}
 };
 
-static PyModuleDef EmbModule = {
-    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods,
+static PyModuleDef KnxModule = {
+    PyModuleDef_HEAD_INIT, "knx", NULL, -1, KnxMethods,
     NULL, NULL, NULL, NULL
 };
 
 static PyObject*
-PyInit_emb(void)
+PyInit_knx(void)
 {
-    return PyModule_Create(&EmbModule);
+    return PyModule_Create(&KnxModule);
 }
 
 int main(void) {
-	PyImport_AppendInittab("emb", PyInit_emb);
+	PyImport_AppendInittab("knx", PyInit_knx);
+
 	Py_Initialize();
+    PyObject *pName = PyUnicode_DecodeFSDefault("simpleclient");
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import os");
+    PyRun_SimpleString("import signal");
+    PyRun_SimpleString("sys.path.append(os.getcwd())");
+
+    PyObject *pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    PyRun_SimpleString("import simpleclient");
+    PyRun_SimpleString("simpleclient.init()");
+
+    while(1)
+    {
+        if (PyRun_SimpleString("signal.sigtimedwait([], 0.1)") != 0)
+        {
+            PyErr_Print();
+            return -1;
+        }
+    }
 	return 0;
 }
