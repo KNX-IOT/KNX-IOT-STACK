80b129b95bbd1d3efcdcbbb9593deed25dfcd382
Wouter <wavdb66@gmail.com>
2022-01-06 17:21:22 +0000

    clean up of python code

--- pythonbinding/application_scripts/install_demo.py
+++ pythonbinding/application_scripts/install_demo.py
@@ -7,30 +7,45 @@
 #    1.  Redistributions of source code must retain the above copyright notice,
 #        this list of conditions and the following disclaimer.
 #    2.  Redistributions in binary form must reproduce the above copyright notice,
-#        this list of conditions and the following disclaimer in the documentation and/or other materials provided
-#        with the distribution.
+#        this list of conditions and the following disclaimer in the documentation
+#        and/or other materials provided with the distribution.
 #
-#    THIS SOFTWARE IS PROVIDED BY THE OPEN CONNECTIVITY FORUM, INC. "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-#    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
-#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE OPEN CONNECTIVITY FORUM, INC. OR
-#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-#    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#    THIS SOFTWARE IS PROVIDED "AS IS"
+#    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
+#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+#    OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS;OR BUSINESS INTERRUPTION)
+#    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 #    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 #    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #############################
+# pylint: disable=C0103
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
+# pylint: disable=C0201
+# pylint: disable=C0209
+# pylint: disable=C0413
+# pylint: disable=R0801
+# pylint: disable=R0902
+# pylint: disable=R0913
+# pylint: disable=R0915
+# pylint: disable=R1732
+# pylint: disable=W0702
+# pylint: disable=W1514
 
-import getopt
-import socket
+import argparse
 import sys
-import cbor
 import json
 import time
 import traceback
 import os
+from  collections import OrderedDict
 import signal
-
 
 #add parent directory to path so we can import from there
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -40,140 +55,169 @@ sys.path.append(parentdir)
 #from knx_stack import KNXIOTStack
 import knx_stack
 
-    
 def do_discover(my_stack):
-  time.sleep(1)
-  devices = my_stack.discover_devices()
-  if my_stack.get_nr_devices() > 0:
-     print ("SN :", my_stack.device_array[0].sn)
-
+    time.sleep(1)
+    my_stack.discover_devices()
+    if my_stack.get_nr_devices() > 0:
+        print ("SN :", my_stack.device_array[0].sn)
 
 def do_install_device(my_stack, sn, ia, iid, fp_content):
-   # sensor, e.g sending  
-   print ("--------------------")
-   print ("Installing SN: ", sn)
-   
-   content = { 2: "reset"}
-   print("reset :", content);
-   response =  my_stack.issue_cbor_post(sn,"/.well-known/knx",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   content = True
-   print("set PM :", content);
-   response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   
-   content = ia
-   print("set IA :", content)
-   response =  my_stack.issue_cbor_put(sn,"/dev/ia",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   content = iid
-   response =  my_stack.issue_cbor_put(sn,"/dev/iid",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   content = { 2: "startLoading"}
-   print("lsm :", content);
-   response =  my_stack.issue_cbor_post(sn,"/a/lsm",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   response =  my_stack.issue_cbor_get(sn,"/a/lsm")
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   
-   # group object table
-   # id (0)= 1
-   # url (11)= /p/light
-   # ga (7 )= 1
-   # cflags (8) = ["r" ] ; read = 1, write = 2, transmit = 3 update = 4
-   content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ] 
-   content = fp_content
-   response =  my_stack.issue_cbor_post(sn,"/fp/g",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   response =  my_stack.issue_linkformat_get(sn,"/fp/g")
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   
-   # recipient table
-   # id (0)= 1
-   # ia (12)
-   # url (11)= .knx
-   # ga (7 )= 1
-   # cflags (8) = ["r" ] ; read = 1, write = 2, transmit = 3 update = 4
-   content = [ {0: 1, 11: "/p/push", 7:[1], 12 :"blah.blah" } ] 
-   response =  my_stack.issue_cbor_post(sn,"/fp/r",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   
-   content = False
-   print("set PM :", content);
-   response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   content = { 2: "loadComplete"}
-   print("lsm :", content);
-   response =  my_stack.issue_cbor_post(sn,"/a/lsm",content)
-   print ("response:",response)
-   my_stack.purge_response(response)
-   
-   response =  my_stack.issue_cbor_get(sn,"/a/lsm")
-   print ("response:",response)
-   my_stack.purge_response(response)
+    # sensor, e.g sending
+    print ("--------------------")
+    print ("Installing SN: ", sn)
 
+    content = { 2: "reset"}
+    print("reset :", content)
+    response =  my_stack.issue_cbor_post(sn,"/.well-known/knx",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
 
-def do_install(my_stack):
+    content = True
+    print("set PM :", content)
+    response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    content = ia
+    print("set IA :", content)
+    response =  my_stack.issue_cbor_put(sn,"/dev/ia",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    content = iid
+    response =  my_stack.issue_cbor_put(sn,"/dev/iid",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    content = { 2: "startLoading"}
+    print("lsm :", content)
+    response =  my_stack.issue_cbor_post(sn,"/a/lsm",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    response =  my_stack.issue_cbor_get(sn,"/a/lsm")
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    # group object table
+    # id (0)= 1
+    # url (11)= /p/light
+    # ga (7 )= 1
+    # cflags (8) = ["r" ] ; read = 1, write = 2, transmit = 3 update = 4
+    content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ]
+    content = fp_content
+    response =  my_stack.issue_cbor_post(sn,"/fp/g",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    response =  my_stack.issue_linkformat_get(sn,"/fp/g")
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    # recipient table
+    # id (0)= 1
+    # ia (12)
+    # url (11)= .knx
+    # ga (7 )= 1
+    # cflags (8) = ["r" ] ; read = 1, write = 2, transmit = 3 update = 4
+    content = [ {0: 1, 11: "/p/push", 7:[1], 12 :"blah.blah" } ]
+    response =  my_stack.issue_cbor_post(sn,"/fp/r",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    content = False
+    print("set PM :", content)
+    response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
+    print ("response:",response)
+    my_stack.purge_response(response)
+
+    content = { 2: "loadComplete"}
+    print("lsm :", content)
+    response =  my_stack.issue_cbor_post(sn,"/a/lsm",content)
+    print ("response:", response)
+    my_stack.purge_response(response)
+
+    response =  my_stack.issue_cbor_get(sn,"/a/lsm")
+    print ("response:", response)
+    my_stack.purge_response(response)
+
+def load_json_file(filename, my_dir=None):
+    """
+    load the JSON schema file
+    :param filename: filename (with extension)
+    :param my_dir: path to the file
+    :return: json_dict
+    """
+    full_path = filename
+    if my_dir is not None:
+        full_path = os.path.join(my_dir, filename)
+    if os.path.isfile(full_path) is False:
+        print("json file does not exist:", full_path)
+    linestring = open(full_path, 'r').read()
+    json_dict = json.loads(linestring, object_pairs_hook=OrderedDict)
+    return json_dict
+
+def do_install(my_stack, filename):
+    json_data = load_json_file(filename)
+    if json_data is None:
+        return
+
     sn = my_stack.device_array[0].sn
     print (" SN : ", sn)
-    iid = "5"  # installation id
-    ia = 0
-    fp_content = []
-    
+    iid = json_data["iid"] # "5"
+    ia = json_data["ia"]
+    fp_content = json_data["got"]
+
     if "000001" == sn :
-       # LSAB
-       ia = 2
-       fp_content = [ { 0: 1, 11: "/p/light", 7:[1], 8: [1] } ] 
+        # LSAB
+        ia = 2
+        fp_content = [ { 0: 1, 11: "/p/light", 7:[1], 8: [1] } ]
     if "000003" == sn :
-       # LSSB
+        # LSSB
         ia = 1
-       fp_content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ] 
-       
-       
+        fp_content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ]
+
     if "000002" == sn :
-       # LSAB-PI
-       ia = 2
-       fp_content = [ { 0: 1, 11: "/p/light", 7:[1], 8: [1] } ] 
+        # LSAB-PI
+        ia = 3
+        fp_content = [ { 0: 1, 11: "/p/light", 7:[1], 8: [1] } ]
     if "000004" == sn :
-       # LSSB-PI
-       ia = 1
-       fp_content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ] 
-       
+        # LSSB-PI
+        ia = 4
+        fp_content = [ {0: 1, 11: "p/push", 7:[1], 8: [2] } ]
     do_install_device(my_stack, sn, ia, iid, fp_content )
-       
-        
+
 if __name__ == '__main__':  # pragma: no cover
 
-    my_stack = knx_stack.KNXIOTStack()
-    signal.signal(signal.SIGINT, my_stack.sig_handler)
-    
-    
-    try: 
-      do_discover(my_stack)
-      do_install(my_stack)
+    parser = argparse.ArgumentParser()
+
+    # input (files etc.)
+    parser.add_argument("-sn", "--serialnumber",
+                    help="serial number of the device", nargs='?',
+                    const=1, required=True)
+    parser.add_argument("-scope", "--scope",
+                    help="scope of the multicast request [2,5]", nargs='?',
+                    default=2, const=1, required=False)
+    parser.add_argument("-file", "--file",
+                    help="filename of the configuration", nargs='?',
+                    default=2, const=1, required=True)
+    # (args) supports batch scripts providing arguments
+    print(sys.argv)
+    args = parser.parse_args()
+
+    print("scope         :" + str(args.scope))
+    print("serial number :" + str(args.serialnumber))
+    print("filename      :" + str(args.file))
+
+    the_stack = knx_stack.KNXIOTStack()
+    signal.signal(signal.SIGINT, the_stack.sig_handler)
+
+    try:
+        do_discover(the_stack)
+        do_install(the_stack, args.filename)
     except:
-      traceback.print_exc()
-     
+        traceback.print_exc()
+
     time.sleep(2)
-    my_stack.quit()
+    the_stack.quit()
     sys.exit()
--- pythonbinding/application_scripts/knx_test_sequence.py
+++ pythonbinding/application_scripts/knx_test_sequence.py
@@ -7,30 +7,42 @@
 #    1.  Redistributions of source code must retain the above copyright notice,
 #        this list of conditions and the following disclaimer.
 #    2.  Redistributions in binary form must reproduce the above copyright notice,
-#        this list of conditions and the following disclaimer in the documentation and/or other materials provided
-#        with the distribution.
+#        this list of conditions and the following disclaimer in the documentation
+#        and/or other materials provided with the distribution.
 #
-#    THIS SOFTWARE IS PROVIDED BY THE OPEN CONNECTIVITY FORUM, INC. "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-#    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
-#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE OPEN CONNECTIVITY FORUM, INC. OR
-#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-#    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#    THIS SOFTWARE IS PROVIDED "AS IS"
+#    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
+#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+#    OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS;OR BUSINESS INTERRUPTION)
+#    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 #    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 #    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #############################
+# pylint: disable=C0103
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
+# pylint: disable=C0201
+# pylint: disable=C0209
+# pylint: disable=C0413
+# pylint: disable=R0801
+# pylint: disable=R0902
+# pylint: disable=R0913
+# pylint: disable=R0915
+# pylint: disable=R1732
+# pylint: disable=W0702
+# pylint: disable=W1514
 
-import getopt
-import socket
-import sys
-import cbor
-import json
-import time
-import traceback
 import os
 import signal
-
+import sys
+import time
+import traceback
 
 #add parent directory to path so we can import from there
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -40,88 +52,14 @@ sys.path.append(parentdir)
 #from knx_stack import KNXIOTStack
 import knx_stack
 
-
-######## old ###################
-
-def do_sequence_knx_spake(my_base):
-
-    #  url, content, accept, contents
-    
-    # sequence:
-    # - parameter exchange: 15 (rnd)- return value
-    # - credential exchange: 10 - return value
-    # - pase verification exchange: 14  - no return value
-    
-    content = { 15: b"a-15-sdfsdred"}
-    execute_post("coap://"+my_base+"/.well-known/knx/spake", 60, 60, content)
-    
-    # pa
-    content = { 10: b"s10dfsdfsfs" }
-    execute_post("coap://"+my_base+"/.well-known/knx/spake", 60, 60, content)
-    
-    # ca
-    content = { 14: b"a15sdfsdred"}
-    execute_post("coap://"+my_base+"/.well-known/knx/spake", 60, 60, content)
-    # expecting return 
-    
-
-def do_sequence_knx_idevid(my_base):
-    #  url, content, accept, contents
-    execute_get("coap://"+my_base+"/.well-known/knx/idevid", 282)
-    
-def do_sequence_knx_ldevid(my_base):
-    #  url, content, accept, contents
-    execute_get("coap://"+my_base+"/.well-known/knx/ldevid", 282)
-
-
-def do_sequence_oscore(my_base):
-    #  url, content, accept, contents
-    execute_get("coap://"+my_base+"/f/oscore", 40)
-    
-    execute_get("coap://"+my_base+"/p/oscore/replwdo", 60)
-    content = 105 
-    execute_put("coap://"+my_base+"/p/oscore/replwdo", 60, 60, content)
-    execute_get("coap://"+my_base+"/p/oscore/replwdo", 60)
-    
-    execute_get("coap://"+my_base+"/p/oscore/osndelay", 60)
-    content =  1050
-    execute_put("coap://"+my_base+"/p/oscore/osndelay", 60, 60, content)
-    execute_get("coap://"+my_base+"/p/oscore/osndelay", 60)
-    
-def do_sequence_auth(my_base):
-    #  url, content, accept, contents
-    
-    execute_get("coap://"+my_base+"/auth", 40)
-    
-    
-def do_sequence_auth_at(my_base):
-    #  url, content, accept, contents
-    
-    execute_get("coap://"+my_base+"/auth/at", 40)
-    # 
-    content = {0: b"id", 1 : 20, 2:b"ms",3:"hkdf", 4:"alg", 5:b"salt", 6:b"contextId"}
-    execute_post("coap://"+my_base+"/auth/at", 60, 60, content)
-    
-    
-    content = {0: b"id2", 1 : 20, 2:b"ms",3:"hkdf", 4:"alg", 5:b"salt", 6:b"contextId2"}
-    execute_post("coap://"+my_base+"/auth/at", 60, 60, content)
-    
-    execute_get("coap://"+my_base+"/auth/at", 40)
-    
-    execute_get("coap://"+my_base+"/auth/at/id", 60)
-    execute_del("coap://"+my_base+"/auth/at/id", 60)
-
-######## working ###################
-
-    
 def test_discover(my_stack):
-  time.sleep(1)
-  devices = my_stack.discover_devices()
-  if my_stack.get_nr_devices() > 0:
-     print ("SN :", my_stack.device_array[0].sn)
+    time.sleep(1)
+    my_stack.discover_devices()
+    if my_stack.get_nr_devices() > 0:
+        print ("SN :", my_stack.device_array[0].sn)
 
 def get_sn(my_stack):
-    print("Get SN :");
+    print("Get SN :")
     sn = my_stack.device_array[0].sn
     response = my_stack.issue_cbor_get(sn, "/dev/sn")
     print ("response:",response)
@@ -131,11 +69,11 @@ def get_sn(my_stack):
 def do_sequence_dev(my_stack):
     sn = my_stack.device_array[0].sn
     print("===================")
-    print("Get HWT :");
+    print("Get HWT :")
     response = my_stack.issue_cbor_get(sn, "/dev/hwt")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     #print("Get HWV :");
     #response =  my_stack.issue_cbor_get(sn, "/dev/hwv")
@@ -149,62 +87,60 @@ def do_sequence_dev(my_stack):
     #my_stack.purge_response(response)
 
     print("===================")
-    print("Get Model :");
+    print("Get Model :")
     response =  my_stack.issue_cbor_get(sn,"/dev/model")
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
     print("===================")
     content = True
-    print("set PM :", content);
+    print("set PM :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/pm")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     content = False
     response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = 44
-    print("set IA :", content);
+    print("set IA :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/ia",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/ia")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = "my host name"
-    print("set hostname :", content);
+    print("set hostname :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/hostname",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/hostname")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = " iid xxx"
-    print("set iid :", content);
-    
+    print("set iid :", content)
+
     response =  my_stack.issue_cbor_put(sn,"/dev/iid",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/iid")
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
 # no json tags as strings
 def do_sequence_dev_programming_mode(my_stack):
@@ -212,55 +148,54 @@ def do_sequence_dev_programming_mode(my_stack):
 
     print("===================")
     content = True
-    print("set PM :", content);
+    print("set PM :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/pm")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = 44
-    print("set IA :", content);
+    print("set IA :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/ia",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/ia")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = "my host name"
-    print("set hostname :", content);
+    print("set hostname :", content)
     response =  my_stack.issue_cbor_put(sn,"/dev/hostname",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/hostname")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     print("===================")
     content = " iid xxx"
-    print("set iid :", content);
-    
+    print("set iid :", content)
+
     response =  my_stack.issue_cbor_put(sn,"/dev/iid",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn,"/dev/iid")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     # reset programming mode
     content = False
     response =  my_stack.issue_cbor_put(sn,"/dev/pm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
 # cmd ==> 2
 def do_sequence_lsm_int(my_stack):
@@ -270,79 +205,68 @@ def do_sequence_lsm_int(my_stack):
     response =  my_stack.issue_cbor_get(sn, "/a/lsm")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     content = {2 : "startLoading"}
     response =  my_stack.issue_cbor_put(sn,"/a/lsm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn, "/a/lsm")
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
     content = {2 : "loadComplete"}
     response =  my_stack.issue_cbor_put(sn,"/a/lsm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn, "/a/lsm")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     content = {2 : "unload"}
     response =  my_stack.issue_cbor_put(sn,"/a/lsm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn, "/a/lsm")
     print ("response:",response)
     my_stack.purge_response(response)
 
-
 # ./knx resource
 # sia ==> 4
 # ga ==> 7
-# st 6 
+# st ==> 6
 def do_sequence_knx_knx_int(my_stack):
     sn = my_stack.device_array[0].sn
-    
     print("========.knx=========")
-    
     response =  my_stack.issue_cbor_get(sn, "/.knx")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     content = {"value": { 4 : 5, 7: 7777 , 6 : "rp"}}
     response =  my_stack.issue_cbor_post(sn,"/a/lsm",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     response =  my_stack.issue_cbor_get(sn, "/.knx")
     print ("response:",response)
     my_stack.purge_response(response)
 
-
 def do_sequence_knx_osn(my_stack):
     sn = my_stack.device_array[0].sn
-    
     print("========knx/osn=========")
-    
     response =  my_stack.issue_cbor_get(sn, "/.well-known/knx/osn")
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
 def do_sequence_knx_crc(my_stack):
     #  url, content, accept, contents
     sn = my_stack.device_array[0].sn
-    
     print("========knx/crc=========")
-    
     response =  my_stack.issue_cbor_get(sn, "/.well-known/knx/crc")
     print ("response:",response)
     my_stack.purge_response(response)
-    
 
 def do_sequence_core_knx(my_stack):
     sn = my_stack.device_array[0].sn
@@ -350,13 +274,12 @@ def do_sequence_core_knx(my_stack):
     response =  my_stack.issue_cbor_get(sn, "/.well-known/knx")
     print ("response:",response)
     my_stack.purge_response(response)
-    
+
     content = { 1 : 5, 2: "reset"}
     response =  my_stack.issue_cbor_post(sn,"/.well-known/knx",content)
     print ("response:",response)
     my_stack.purge_response(response)
-    
-    
+
 def do_sequence_a_sen(my_stack):
     sn = my_stack.device_array[0].sn
     print("========/a/sen=========")
@@ -364,11 +287,9 @@ def do_sequence_a_sen(my_stack):
     response =  my_stack.issue_cbor_post(sn,"/a/sen",content)
     print ("response:", response)
     my_stack.purge_response(response)
-    
-    
+
 def do_sequence_f(my_stack):
     sn = my_stack.device_array[0].sn
-    
     print("========/f=========")
     response =  my_stack.issue_linkformat_get(sn, "/f")
     print ("response:", response)
@@ -380,44 +301,36 @@ def do_sequence_f(my_stack):
         print(" ct  :", lf.get_ct(line))
         print(" ct  :", lf.get_rt(line))
         if lf.get_ct(line) == "40" :
-           response2 =  my_stack.issue_cbor_get(sn, lf.get_url(line))
-           print ("response2:",response2)
-           my_stack.purge_response(response2)
+            response2 =  my_stack.issue_cbor_get(sn, lf.get_url(line))
+            print ("response2:",response2)
+            my_stack.purge_response(response2)
 
     my_stack.purge_response(response)
-    
 
 def do_sequence(my_stack):
-    
-    sn = get_sn(my_stack)
-    do_sequence_dev(my_stack)
-    do_sequence_dev_programming_mode(my_stack)
-       
-    do_sequence_lsm_int(my_stack)
-    # .knx
-    do_sequence_knx_knx_int(my_stack)
-    
-    do_sequence_knx_crc(my_stack)
-    do_sequence_knx_osn(my_stack)
-    
-    do_sequence_core_knx(my_stack)
-    
-    do_sequence_a_sen(my_stack)
-        
-    do_sequence_f(my_stack)
-        
-        
-if __name__ == '__main__':  # pragma: no cover
+    if my_stack.get_nr_devices() > 0:
+        get_sn(my_stack)
+        do_sequence_dev(my_stack)
+        do_sequence_dev_programming_mode(my_stack)
+        do_sequence_lsm_int(my_stack)
+        # .knx
+        do_sequence_knx_knx_int(my_stack)
+        do_sequence_knx_crc(my_stack)
+        do_sequence_knx_osn(my_stack)
+        do_sequence_core_knx(my_stack)
+        do_sequence_a_sen(my_stack)
+        do_sequence_f(my_stack)
 
-    my_stack = knx_stack.KNXIOTStack()
-    signal.signal(signal.SIGINT, my_stack.sig_handler)
-    
-    try: 
-      test_discover(my_stack)
-      do_sequence(my_stack)
+if __name__ == '__main__':  # pragma: no cover
+    the_stack = knx_stack.KNXIOTStack()
+    signal.signal(signal.SIGINT, the_stack.sig_handler)
+
+    try:
+        test_discover(the_stack)
+        do_sequence(the_stack)
     except:
-      traceback.print_exc()
-    
+        traceback.print_exc()
+
     time.sleep(2)
-    my_stack.quit()
+    the_stack.quit()
     sys.exit()
