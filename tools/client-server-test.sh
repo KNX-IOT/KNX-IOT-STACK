aefd77488481ae384543b10b3a037010a4e2d786
Wouter <wavdb66@gmail.com>
2022-01-18 16:51:53 +0000

    sleep in batch then kill processes

--- tools/client-server-test.sh
+++ tools/client-server-test.sh
@@ -9,9 +9,16 @@ cp ./libkisCS.so ../pythonbinding/
 
 cd ../pythonbinding/tests
 python knx_test_sequence.py -sleep 2 &
+pythonPID=$!
 ps
 
 ls ../../linuxbuild_clientserver/apps
 
-../../linuxbuild_clientserver/apps/simpleserver_all 4
+../../linuxbuild_clientserver/apps/simpleserver_all &
+serverPID=$!
+
+sleep 20
+
+kill $pythonPID
+kill $serverPID
 
