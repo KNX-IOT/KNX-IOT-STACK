034bbb55991be03ee652bcbdab74042da37a2f9a
Wouter <wavdb66@gmail.com>
2022-01-18 17:12:01 +0000

    using if for grep return code

--- tools/client-server-test.sh
+++ tools/client-server-test.sh
@@ -8,6 +8,9 @@ python -m pip install -r ../pythonbinding/requirements.txt
 
 cp ./libkisCS.so ../pythonbinding/
 
+
+echo "--Starting Python----"
+
 cd ../pythonbinding/tests
 python knx_test_sequence.py -sleep > $mydir/python_out.txt 2>&1 2&
 pythonPID=$!
@@ -15,6 +18,8 @@ ps
 
 ls ../../linuxbuild_clientserver/apps
 
+
+echo "--Starting simpleserver_all----"
 ../../linuxbuild_clientserver/apps/simpleserver_all &
 serverPID=$!
 
@@ -23,6 +28,11 @@ sleep 60
 kill $pythonPID
 kill $serverPID
 
-echo "---------"
-grep Failure $mydir/python_out.txt
+echo "---Collecting Failures------"
+
+if grep -q$mydir/python_out.txt; then
+    echo Failures found
+else
+    echo no failures found
+fi
 
