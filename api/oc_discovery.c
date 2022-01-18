1ab625fa34d04e2677f6e62b3197f7727b396392
Wouter <wavdb66@gmail.com>
2022-01-18 11:31:42 +0000

    removed unused variable

--- api/oc_discovery.c
+++ api/oc_discovery.c
@@ -253,7 +253,7 @@ oc_wkcore_discovery_handler(oc_request_t *request,
       return;
     }
     // create the response
-    bool added = oc_add_points_in_group_object_table_to_response(
+    oc_add_points_in_group_object_table_to_response(
       request, device_index, group_address, &response_length, matches);
     request->response->response_buffer->response_length = response_length;
     request->response->response_buffer->code = oc_status_code(OC_STATUS_OK);
