b1d1feed419dc3eeb94a713c69562305d59e111a
Wouter <wavdb66@gmail.com>
2022-01-21 15:36:39 +0000

    increase size of print buffer

--- api/oc_knx_fb.c
+++ api/oc_knx_fb.c
@@ -159,7 +159,7 @@ oc_add_function_blocks_to_response(oc_request_t *request, size_t device_index,
 {
   (void)request;
   int length = 0;
-  char number[6];
+  char number[24];
   int i;
 
   // use global variable
@@ -196,7 +196,7 @@ oc_add_function_blocks_to_response(oc_request_t *request, size_t device_index,
     length = oc_rep_add_line_to_buffer("</f/");
     *response_length += length;
     if (g_int_array[1][i] > 0) {
-      snprintf(number, 6, "%3d_%2d", g_int_array[0][i], g_int_array[1][i]);
+      snprintf(number, 23, "%3d_%2d", g_int_array[0][i], g_int_array[1][i]);
     } else {
       snprintf(number, 5, "%d", g_int_array[0][i]);
     }
