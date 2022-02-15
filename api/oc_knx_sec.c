46b294f27df9e87515b761effe5e513f2a72e62d
Wouter <wavdb66@gmail.com>
2022-02-15 14:02:42 +0000

    fixes

--- api/oc_knx_sec.c
+++ api/oc_knx_sec.c
@@ -1345,7 +1345,7 @@ oc_knx_contains_interface(oc_interface_mask_t at_interface, oc_interface_mask_t 
   oc_interface_mask_t new_mask;
   oc_interface_mask_t at_mask;
   oc_interface_mask_t resource_mask;
-  PRINT("oc_knx_contains_interface   %d %d  \n", at_interface, resource_interface);
+  PRINT("------ oc_knx_contains_interface  at  %d resource %d  \n", at_interface, resource_interface);
   for (i = 1; i < OC_MAX_IF_MASKS + 1; i++) {
     new_mask = 1 << i;
     at_mask = at_interface && new_mask;
--- api/unittest/apitest.cpp
+++ api/unittest/apitest.cpp
@@ -54,14 +54,22 @@ TEST(KNXLSM, LSMConstToStr)
 
 TEST(KNXSEC, contains_interfaces) 
 {
-  bool check;
   EXPECT_FALSE(oc_knx_contains_interface(OC_IF_NONE, OC_IF_NONE));
   
   EXPECT_TRUE(oc_knx_contains_interface(OC_IF_I, OC_IF_I));
+  EXPECT_TRUE(oc_knx_contains_interface(OC_IF_I, OC_IF_I | OC_IF_O));
+  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_I, OC_IF_NONE));
   EXPECT_FALSE(oc_knx_contains_interface(OC_IF_I, OC_IF_O));
-  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_I, OC_IF_I | OC_IF_O));
   
   EXPECT_TRUE(oc_knx_contains_interface(OC_IF_O, OC_IF_O));
+  EXPECT_TRUE(oc_knx_contains_interface(OC_IF_O, OC_IF_O | OC_IF_G));
+  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_O, OC_IF_NONE));
   EXPECT_FALSE(oc_knx_contains_interface(OC_IF_O, OC_IF_I));
   EXPECT_FALSE(oc_knx_contains_interface(OC_IF_O, OC_IF_I | OC_IF_G));
+  
+  EXPECT_TRUE(oc_knx_contains_interface(OC_IF_M, OC_IF_M));
+  EXPECT_TRUE(oc_knx_contains_interface(OC_IF_M, OC_IF_M | OC_IF_G));
+  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_M, OC_IF_NONE));
+  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_M, OC_IF_I));
+  EXPECT_FALSE(oc_knx_contains_interface(OC_IF_M, OC_IF_I | OC_IF_G));
 }
