3aec458635530e99a1e743790795e8d3e04c674d
Alex Tiniuc <a.tiniuc@cascoda.com>
2022-04-06 13:37:28 +0100

    proof of concept windows implementation

--- port/windows/dns-sd.c
+++ port/windows/dns-sd.c
@@ -1,8 +1,19 @@
+#include "dns-sd.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <process.h>
+
+intptr_t process_handle;
+
 int
-knx_publish_service(char *serial_no, char *iid, char *ia)
+knx_publish_service(char *serial_no, uint32_t iid, uint32_t ia)
 {
-  // unimplemented
   (void)serial_no;
   (void)iid;
   (void)ia;
+
+  process_handle = _spawnlp(_P_NOWAIT, "dns-sd",
+    "dns-sd", "-R", "1234", "_knx._udp,_12345,_ia0", "local", "5447", NULL
+  );
+  return 0;
 }
