09e4eaa7c82faba6853cd92da51010bac935f679
Alex Tiniuc <a.tiniuc@cascoda.com>
2022-04-06 16:04:39 +0100

    windows implementation of dnssd

--- port/windows/dns-sd.c
+++ port/windows/dns-sd.c
@@ -2,8 +2,10 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <process.h>
+#include <string.h>
 
-intptr_t process_handle;
+intptr_t process_handle = 0;
+char subtypes[200];
 
 int
 knx_publish_service(char *serial_no, uint32_t iid, uint32_t ia)
@@ -12,8 +14,36 @@ knx_publish_service(char *serial_no, uint32_t iid, uint32_t ia)
   (void)iid;
   (void)ia;
 
-  process_handle = _spawnlp(_P_NOWAIT, "dns-sd",
-    "dns-sd", "-R", "1234", "_knx._udp,_12345,_ia0", "local", "5447", NULL
-  );
+  if (process_handle != 0)
+  {
+    TerminateProcess(process_handle, 0);
+  }
+
+  if (iid == 0 || ia == 0)
+  {
+    sprintf(subtypes, "_knx._udp,_%s,_ia0", serial_no);
+    process_handle = _spawnlp(_P_NOWAIT, "dns-sd",
+      "dns-sd",
+      "-R", 
+      serial_no,
+      subtypes,
+      "local", 
+      "5683", 
+      NULL
+    );
+  }
+  else
+  {
+    sprintf(subtypes, "_knx._udp,_%s,_ia%X-%X", serial_no, iid, ia);
+    process_handle = _spawnlp(_P_NOWAIT, "dns-sd",
+      "dns-sd",
+      "-R", 
+      serial_no,
+      subtypes,
+      "local", 
+      "5683", 
+      NULL
+    );
+  }
   return 0;
 }
