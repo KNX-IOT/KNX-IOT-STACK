caeb06e64c697f8acceb85ea9639aa9fe36a18d1
Alex Tiniuc <a.tiniuc@cascoda.com>
2022-04-11 10:30:59 +0100

    Revert "statically implement allocate_events"
    This reverts commit b69912ddeb0dd25ade3d3432ffe525988fd193e8.

--- util/oc_process.c
+++ util/oc_process.c
@@ -47,6 +47,8 @@
 struct oc_process *oc_process_list = NULL;
 struct oc_process *oc_process_current = NULL;
 
+static oc_process_event_t lastevent;
+
 /*
  * Structure used for keeping the queue of active events.
  */
@@ -83,6 +85,13 @@ static volatile unsigned char poll_requested;
 static void call_process(struct oc_process *p, oc_process_event_t ev,
                          oc_process_data_t data);
 
+/*---------------------------------------------------------------------------*/
+oc_process_event_t
+oc_process_alloc_event(void)
+{
+  return lastevent++;
+}
+/*---------------------------------------------------------------------------*/
 void
 oc_process_start(struct oc_process *p, oc_process_data_t data)
 {
@@ -200,7 +209,15 @@ oc_process_init(void)
   }
 #endif /* OC_DYNAMIC_ALLOCATION */
 
-  return;
+  lastevent = OC_PROCESS_EVENT_MAX;
+
+  //nevents = fevent = 0;
+#if OC_PROCESS_CONF_STATS
+  process_maxevents = 0;
+#endif /* OC_PROCESS_CONF_STATS */
+
+  //oc_process_current = NULL;
+  //oc_process_list = NULL;
 }
 /*---------------------------------------------------------------------------*/
 /*
