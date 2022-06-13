5b21aced688bc39c91a5c06fa13b5f4b22692669
Wouter <wavdb66@gmail.com>
2022-06-13 15:23:12 +0100

    enabling publisher & recipient table

--- api/oc_knx_fp.h
+++ api/oc_knx_fp.h
@@ -150,6 +150,7 @@ typedef struct oc_group_object_table_t
  * | -------- | ------------- |
  * | id       | 0             |
  * | ia       | 12            |
+ * | grpid    | 13            |
  * | path     | 112           |
  * | url      | 10            |
  * | ga       | 7             |
@@ -158,21 +159,24 @@ typedef struct oc_group_object_table_t
  * The structure stores the information.
  * The structure will be used as an array.
  * There are function to find:
+ * - max amount of entries
  * - empty index in the array
  * - find the index with a specific id
  * - delete an index, e.g. delete the array entry of data
  * - make the entry persistent
  * - free up the allocated data
+ * - return the structure at a specific index
  */
 typedef struct oc_group_rp_table_t
 {
   int id;           /**< contents of id*/
   int ia;           /**< contents of ia (internal address)*/
+  int grpid;        /**< the multicast group id */
   oc_string_t path; /**< contents of path, default path = ".knx"*/
-  oc_string_t url;  /**< contents of url*/
+  oc_string_t url;  /**< contents of url */
   bool con;         /**< confirmed message, default = false*/
-  int *ga;          /**< array of integers*/
-  int ga_len;       /**< length of the array of ga identifiers*/
+  int *ga;          /**< array of integers */
+  int ga_len;       /**< length of the array of ga identifiers */
 } oc_group_rp_table_t;
 
 /**
@@ -372,6 +376,29 @@ int oc_core_get_recipient_ia(int index);
  * @return int the size of the table
  */
 int oc_core_get_recipient_table_size();
+
+/**
+ * @brief retrieve the recipient table entry
+ *
+ * @param index the index in the recipient table
+ * @return oc_group_rp_table_t* pointer to the entry
+ */
+oc_group_rp_table_t *oc_core_get_recipient_table_entry(int index);
+
+/**
+ * @brief return the size of the publisher table
+ *
+ * @return int the size of the table
+ */
+int oc_core_get_publisher_table_size();
+
+/**
+ * @brief retrieve the publisher table entry
+ *
+ * @param index the index in the publisher table
+ * @return oc_group_rp_table_t* pointer to the entry
+ */
+oc_group_rp_table_t *oc_core_get_publisher_table_entry(int index);
 
 /**
  * @brief add points to the well-known core discovery response
