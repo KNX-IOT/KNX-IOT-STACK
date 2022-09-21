f482e01a836c6b21e1c84a1d7071c789e88945ab
Alex Tiniuc <a.tiniuc@cascoda.com>
2022-09-21 16:27:04 +0100

    another try of encoding empty payload

--- api/oc_knx.c
+++ api/oc_knx.c
@@ -1454,7 +1454,8 @@ static oc_event_callback_retval_t oc_core_knx_spake_separate_post_handler(void *
     oc_oscore_set_auth(oc_string(device->serialnumber), oc_string(g_pase.id),
                        shared_key, (int)shared_key_len);
 
-    spake_separate_rsp.response_state->payload_size = 0;
+    // empty payload
+    oc_rep_encode_raw(NULL, 0);
     oc_send_separate_response(&spake_separate_rsp, OC_STATUS_CHANGED);
 
     // handshake completed successfully - clear state
