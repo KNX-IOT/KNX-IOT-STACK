424ca2b537dccfbdbb19f8ff3a974d4f06510577
Elie El Khoury <e.khoury@cascoda.com>
2023-01-19 15:38:28 +0000

    Fixes delayed response behaviour

--- apps/testserver_all.c
+++ apps/testserver_all.c
@@ -958,6 +958,24 @@ hostname_cb(size_t device_index, oc_string_t host_name, void *data)
   PRINT("-----host name ------- %s\n", oc_string_checked(host_name));
 }
 
+static oc_event_callback_retval_t send_delayed_response(void *context)
+{
+  oc_separate_response_t *response = (oc_separate_response_t *)context;
+
+  if (response->active)
+  {
+    oc_set_separate_response_buffer(response);
+    oc_send_separate_response(response, OC_STATUS_CHANGED);
+    printf("Delayed response sent\n");
+  }
+  else
+  {
+    printf("Delayed response NOT active\n");
+  }
+
+  return OC_EVENT_DONE;
+}
+
 /* separate files for each call to transport a block of data*/
 void
 swu_cb(size_t device, oc_separate_response_t *response, size_t binary_size,
@@ -972,8 +990,7 @@ swu_cb(size_t device, oc_separate_response_t *response, size_t binary_size,
   size_t r = fwrite(payload, sizeof(*payload), len, write_ptr);
   fclose(write_ptr);
 
-  oc_set_separate_response_buffer(response);
-  oc_send_separate_response(response, OC_STATUS_CHANGED);
+  oc_set_delayed_callback(response, &send_delayed_response, 0);
 }
 
 /**
