1c8d5e1406a1a4c6e9410d408dcc4ba3e1d0c277
Wouter <wavdb66@gmail.com>
2023-05-11 10:03:31 +0100

    reporting application list via /ap

--- include/oc_ri.h
+++ include/oc_ri.h
@@ -257,7 +257,7 @@ typedef enum {
   OC_DEV_MPORT,             /**< /dev/mport the multicast port number */
   OC_DEV_MID,               /**< /dev/mid the manufacturer id */
   OC_DEV,                   /**< core link */
-  OC_APP,                   /**< application id (list( */
+  OC_APP,                   /**< application id (list) */
   OC_APP_X,                 /**< application id entry */
   OC_KNX_SPAKE,             /**< spake */
   OC_KNX_IDEVID,            /**< IDevID */
