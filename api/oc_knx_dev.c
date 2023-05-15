90d7ecbb366424be5e43fc299b72d927ac4ce51c
Wouter <wavdb66@gmail.com>
2023-05-15 16:21:44 +0100

    disable put handlers for /dev/port and /dev/mport

--- api/oc_knx_dev.c
+++ api/oc_knx_dev.c
@@ -869,7 +869,7 @@ oc_create_dev_port_resource(int resource_idx, size_t device)
   oc_core_populate_resource(resource_idx, device, "/dev/port", OC_IF_P,
                             APPLICATION_CBOR, OC_DISCOVERABLE,
                             oc_core_dev_port_get_handler,
-                            oc_core_dev_port_put_handler, 0, 0, 0);
+                            0, 0, 0, 0);
 
   oc_core_bind_dpt_resource(resource_idx, device, "urn:knx:dpt.value2Ucount");
 }
@@ -945,7 +945,7 @@ oc_create_dev_mport_resource(int resource_idx, size_t device)
   oc_core_populate_resource(resource_idx, device, "/dev/mport", OC_IF_P,
                             APPLICATION_CBOR, OC_DISCOVERABLE,
                             oc_core_dev_mport_get_handler,
-                            oc_core_dev_mport_put_handler, 0, 0, 0);
+                            0, 0, 0, 0);
 
   oc_core_bind_dpt_resource(resource_idx, device, "urn:knx:dpt.value2Ucount");
 }
