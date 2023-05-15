7a95f8344d939f19f51842df85ead2198985bc8d
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2023-05-15 14:03:57 +0100

    remove 'bonus' null terminator added to recipient ID
    Looks like the string we get from the oc_rep_t is
    the byte string on the wire, plus the null termi-
    nator, so that you can treat it as a C-style string.
    
    oc_string_size gets rid of this terminator and returns
    the actual payload on the wire.

--- api/oc_knx.c
+++ api/oc_knx.c
@@ -1309,7 +1309,7 @@ oc_core_knx_spake_post_handler(oc_request_t *request,
     // this gets overwritten if the ID is present in the
     // request payload handled below
     oc_free_string(&g_pase.id);
-    oc_new_string(&g_pase.id, "responderkey", strlen("responderkey"));
+    oc_new_byte_string(&g_pase.id, "responderkey", strlen("responderkey"));
   }
   // handle input
   while (rep != NULL) {
@@ -1331,7 +1331,7 @@ oc_core_knx_spake_post_handler(oc_request_t *request,
         // if the ID is present, overwrite the default
         oc_free_string(&g_pase.id);
         oc_new_byte_string(&g_pase.id, oc_string(rep->value.string),
-                      oc_byte_string_len(rep->value.string));
+                      oc_string_len(rep->value.string));
         PRINT("==> CLIENT RECEIVES %d\n", (int)oc_byte_string_len(rep->value.string));
       }
     } break;
