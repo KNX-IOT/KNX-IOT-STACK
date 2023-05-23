1ae03d9a074dcd2d10b7d23c774a770aa5e9b119
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2023-05-23 15:27:48 +0100

    fix build error

--- api/oc_server_api.c
+++ api/oc_server_api.c
@@ -162,6 +162,7 @@ oc_set_delayed_callback(void *cb_data, oc_trigger_t callback, uint16_t seconds)
   oc_ri_add_timed_event_callback_seconds(cb_data, callback, seconds);
 }
 
+void
 oc_set_delayed_callback_ms(void *cb_data, oc_trigger_t callback, uint16_t miliseconds)
 {
   oc_ri_add_timed_event_callback_ticks(cb_data, callback, miliseconds);
--- include/oc_api.h
+++ include/oc_api.h
@@ -1699,7 +1699,7 @@ void oc_set_delayed_callback(void *cb_data, oc_trigger_t callback,
  * @param[in] callback the callback invoked after the set number of seconds
  * @param[in] miliseconds the number of miliseconds to wait till the callback is invoked
  */
-oc_set_delayed_callback_ms(void *cb_data, oc_trigger_t callback,
+void oc_set_delayed_callback_ms(void *cb_data, oc_trigger_t callback,
                           uint16_t miliseconds);
 
 /**
