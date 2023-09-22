253c0139a7ed259c13a14310d9575ec39ac63288
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2023-09-22 10:23:39 +0100

    refactor SSN logic & include PIV when necessary

--- security/oc_oscore_engine.c
+++ security/oc_oscore_engine.c
@@ -845,15 +845,19 @@ oc_oscore_send_message(oc_message_t *msg)
        * SSN */
       coap_transaction_t *transaction =
         coap_get_transaction_by_token(coap_pkt->token, coap_pkt->token_len);
-      
-      if (transaction && transaction->retrans_counter == 0)
-        increment_ssn_in_context(oscore_ctx);
-      else if (coap_pkt->type == COAP_TYPE_ACK && coap_pkt->token_len == 0)
-        increment_ssn_in_context(oscore_ctx);
-      else if (coap_pkt->type == COAP_TYPE_CON)
-        increment_ssn_in_context(oscore_ctx);
-      else if (!transaction)
-        increment_ssn_in_context(oscore_ctx);
+
+      {
+        bool is_initial_transmission = transaction && transaction->retrans_counter == 0;
+        bool is_empty_ack            =  coap_pkt->type == COAP_TYPE_ACK && coap_pkt->token_len == 0;
+        bool is_separate_response    = coap_pkt->type == COAP_TYPE_CON;
+        bool is_not_transaction = !transaction;
+
+        if  (is_initial_transmission
+          || is_empty_ack
+          || is_separate_response
+          || is_not_transaction)
+          increment_ssn_in_context(oscore_ctx);
+      }
 
       /* Compute nonce using partial IV and sender ID of the sender ( = receiver
        * ID )*/
@@ -946,13 +950,15 @@ oc_oscore_send_message(oc_message_t *msg)
       coap_set_header_max_age(coap_pkt, 0);
     }
 
+    bool is_request = coap_pkt->type == COAP_TYPE_CON && coap_pkt->type == COAP_TYPE_NON;
+    bool is_empty_ack            =  coap_pkt->type == COAP_TYPE_ACK && coap_pkt->token_len == 0;
+    bool is_separate_response    = coap_pkt->type == COAP_TYPE_CON;
     /* Set the OSCORE option */
-    if ((coap_pkt->code >= OC_GET && coap_pkt->code <= OC_DELETE)) {
-      // requests encode the PIV
+    if (is_request || is_empty_ack || is_separate_response) {
       coap_set_header_oscore(coap_pkt, piv, piv_len, kid, kid_len, ctx_id,
                              ctx_id_len);
     } else {
-      // responses use the (cached) piv of the matching request, stored in the
+      // other responses use the (cached) piv of the matching request, stored in the
       // ep/clientcb
       coap_set_header_oscore(coap_pkt, NULL, 0, kid, kid_len, ctx_id,
                              ctx_id_len);
