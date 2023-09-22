89c53764e6516b94bead6c2fa84d45065dfbd1ce
Alexandru Tiniuc <tiniuc.alexandru@gmail.com>
2023-09-22 10:14:34 +0100

    increment context for empty ACKs & separate responses

--- security/oc_oscore_engine.c
+++ security/oc_oscore_engine.c
@@ -836,11 +836,21 @@ oc_oscore_send_message(oc_message_t *msg)
       }
       OC_DBG("### protecting outgoing response ###");
 
+      /* Use context->SSN as partial IV */
+      oscore_store_piv(oscore_ctx->ssn, piv, &piv_len);
+      OC_DBG_OSCORE("---using SSN as Partial IV: %lu", oscore_ctx->ssn);
+      OC_LOGbytes_OSCORE(piv, piv_len);
+      OC_DBG_OSCORE("---");
       /* Increment SSN for the original request, retransmissions use the same
        * SSN */
       coap_transaction_t *transaction =
         coap_get_transaction_by_token(coap_pkt->token, coap_pkt->token_len);
+      
       if (transaction && transaction->retrans_counter == 0)
+        increment_ssn_in_context(oscore_ctx);
+      else if (coap_pkt->type == COAP_TYPE_ACK && coap_pkt->token_len == 0)
+        increment_ssn_in_context(oscore_ctx);
+      else if (coap_pkt->type == COAP_TYPE_CON)
         increment_ssn_in_context(oscore_ctx);
       else if (!transaction)
         increment_ssn_in_context(oscore_ctx);
