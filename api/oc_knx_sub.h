a8a23a0b19022a5849b2f609bbfb04a06d14f67c
Frank Zhong <f.zhong@cascoda.com>
2023-10-24 11:16:17 +0100

    Add new files for /sub

---
+++ api/oc_knx_sub.h
@@ -0,0 +1,23 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+// Copyright (c) 2021-2023 Cascoda Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_helpers.h"
+#include "oc_ri.h"
+#include "oc_core_res.h"
+#include "oc_api.h"
+
+void oc_create_sub_resource(int resource_idx, size_t device);
