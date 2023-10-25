73e59ef996cdf13bbdf24e4ee348f49f6a473471
Frank Zhong <f.zhong@cascoda.com>
2023-10-25 16:41:54 +0100

    Fix copyright

--- api/oc_knx_sub.h
+++ api/oc_knx_sub.h
@@ -1,6 +1,5 @@
 /*
-// Copyright (c) 2016 Intel Corporation
-// Copyright (c) 2021-2023 Cascoda Ltd
+// Copyright (c) 2023 Cascoda Ltd
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
