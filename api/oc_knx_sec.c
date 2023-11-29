7df3ddc75b9cbb9f48c73f821fc6145b058218cc
Frank Zhong <f.zhong@cascoda.com>
2023-11-29 17:28:43 +0000

    Implement pagination for /auth/o

--- api/oc_knx_sec.c
+++ api/oc_knx_sec.c
@@ -239,6 +239,15 @@ oc_core_knx_auth_o_get_handler(oc_request_t *request,
   int i;
   int matches = 0;
 
+  bool ps_exists = false;
+  bool total_exists = false;
+  int total = (int)OC_KNX_AUTH_O_OSNDELAY - (int)OC_KNX_AUTH_O_REPLWDO;
+  int first_entry = (int)OC_KNX_AUTH_O_REPLWDO; // inclusive
+  int last_entry = (int)OC_KNX_AUTH_O; // exclusive
+  // int query_ps = -1;
+  int query_pn = -1;
+  bool more_request_needed = false; // If more requests (pages) are needed to get the full list
+
   /* check if the accept header is cbor-format */
   if (oc_check_accept_header(request, APPLICATION_LINK_FORMAT) == false) {
     request->response->response_buffer->code =
@@ -248,8 +257,29 @@ oc_core_knx_auth_o_get_handler(oc_request_t *request,
 
   size_t device_index = request->resource->device;
 
-  for (i = (int)OC_KNX_AUTH_O_REPLWDO; i <= (int)OC_KNX_AUTH_O_OSNDELAY;
-       i++) {
+  // handle query parameters: l=ps l=total
+  if (check_if_query_l_exist(request, &ps_exists, &total_exists)) {
+    // example : < /dev > l = total>;total=22;ps=5
+    response_length = oc_frame_query_l(oc_string(request->resource->uri), ps_exists, PAGE_SIZE, total_exists, total);
+    oc_send_linkformat_response(request, OC_STATUS_OK, response_length);
+    return;
+  }
+
+  // handle query with page number (pn)
+  if (check_if_query_pn_exist(request, &query_pn, NULL)) {
+    first_entry += query_pn * PAGE_SIZE;
+    if (first_entry >= last_entry) {
+      oc_send_response_no_format(request, OC_STATUS_BAD_REQUEST);
+      return;
+    }
+  }
+
+  if (last_entry > first_entry + PAGE_SIZE) {
+    last_entry = first_entry + PAGE_SIZE;
+    more_request_needed = true;
+  }
+
+  for (i = first_entry; i < last_entry; i++) {
     const oc_resource_t *resource =
       oc_core_get_resource_by_index(i, device_index);
     if (oc_filter_resource(resource, request, device_index, &response_length, &i, i)) {
@@ -258,6 +288,10 @@ oc_core_knx_auth_o_get_handler(oc_request_t *request,
   }
 
   if (matches > 0) {
+    if (more_request_needed) {
+      int next_page_num = query_pn > -1 ? query_pn + 1 : 1;
+      response_length += add_next_page_indicator(oc_string(request->resource->uri), next_page_num);
+    }
     oc_send_linkformat_response(request, OC_STATUS_OK, response_length);
   } else {
     oc_send_response_no_format(request, OC_STATUS_INTERNAL_SERVER_ERROR);
