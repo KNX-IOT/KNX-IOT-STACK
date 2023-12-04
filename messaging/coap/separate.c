c8551e646659de2384b9f767b8bc8d74ef684b0c
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2023-12-04 11:35:25 +0000

    only remove token for the separate ack

--- messaging/coap/separate.c
+++ messaging/coap/separate.c
@@ -114,11 +114,8 @@ coap_separate_accept(void *request, oc_separate_response_t *separate_response,
     /* store correct response type */
     separate_store->type = COAP_TYPE_CON;
 
-    // only include token for secured responses
-    if (endpoint->flags & OSCORE){
-      memcpy(separate_store->token, coap_req->token, coap_req->token_len);
-      separate_store->token_len = coap_req->token_len;
-    }
+    memcpy(separate_store->token, coap_req->token, coap_req->token_len);
+    separate_store->token_len = coap_req->token_len;
 
     oc_new_string(&separate_store->uri, coap_req->uri_path,
                   coap_req->uri_path_len);
@@ -138,11 +135,13 @@ coap_separate_accept(void *request, oc_separate_response_t *separate_response,
   if (coap_req->type == COAP_TYPE_CON) {
     OC_DBG("Sending ACK for separate response");
     coap_packet_t ack[1];
-    /* ACK with empty code (0) */
+    // ACK with empty code (0)
+    // only include token for secured responses
     coap_udp_init_message(ack, COAP_TYPE_ACK, 0, coap_req->mid);
-    ack->token_len = separate_store->token_len;
-    memcpy(ack->token, separate_store->token, ack->token_len);
-
+    if (endpoint->flags & OSCORE){
+      ack->token_len = separate_store->token_len;
+      memcpy(ack->token, separate_store->token, ack->token_len);
+    }
     oc_message_t *message = oc_internal_allocate_outgoing_message();
     if (message != NULL) {
       memcpy(&message->endpoint, endpoint, sizeof(oc_endpoint_t));
